6ac4097483d97d863d2bf8f03dc150682b0b1d99
Aditya Gupta <agupta21@umd.edu>
2021-11-24 00:46:47 -0500

    working commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 // src/App.js
 
 import React from "react";
-import About from "components/About.js";
-import Projects from "components/Projects.js";
-import Navbar from "components/Navbar.js";
-import Skills from "components/Skills.js"
-import Contact from "components/Contact.js";
+import About from "./components/About.js"
+import Intro from "./components/Intro.js";
+import Projects from "./components/Projects.js";
+import Navbar from "./components/Navbar.js";
+import Skills from "./components/Skills.js"
+import Contact from "./components/Contact.js";
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
+      <Intro />
       <About />
       <Projects />
       <Skills />
--- src/components/About.js
+++ src/components/About.js
@@ -1,1 +1,31 @@
-export default function About() {}
+import React from "react";
+
+export default function About() {
+    return (
+        <section id="about">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                        Bio
+                        <br className="hidden lg:inline-block" />
+                    </h1>
+                    <p className="mb-8 leading-relaxed">
+                        My life to this date has been defined by both experiences and beliefs. 
+                    </p>
+
+                    <h1 className="title-font sm:text-2xl text-2xl mb-4 font-medium text-white">
+                        Hobbies
+                        <br className="hidden lg:inline-block" />
+                    </h1>
+                </div>
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                    <img
+                        className="object-cover object-center rounded"
+                        alt="hero"
+                        src="./IntroPic1.jpg"
+                    />
+                </div>
+            </div>
+        </section>
+    );
+}
