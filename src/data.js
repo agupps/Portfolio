0c12b3279e8690ec1dd82d11136724324c560c2f
Aditya Gupta <agupta21@umd.edu>
2021-11-26 22:12:05 -0500

    added more projects

--- src/data.js
+++ src/data.js
@@ -3,9 +3,41 @@ export const projects = [
     title: "The Blinds",
     subtitle: "Mechatronics CGI&RPi",
     description:
-      "Here, a remote controlled window shutters are built and programmed with sensor and API input. The device makes decisions on whether to open or close the shutters.",
+      "Remote controlled window shutters are built and programmed with sensor and API input. The device makes decisions on whether to open or close the shutters.",
     image: "./TheBlinds.jpg",
     link: "https://youtu.be/4NyuF9Z8qmg",
+  },
+  {
+    title: "Auto Toilet Paper Dispenser",
+    subtitle: "Simple I/O Ardunio Project",
+    description:
+      "An automatic toilet paper dispenser programmed with Arduino with visual information displayed with Processing.",
+    image: "./ToiletPaperDispenser.jpg",
+    link: "https://youtu.be/aCX7mcY8LEc",
+  },
+  {
+    title: "Election Turnout Analysis",
+    subtitle: "Data Science Analysis with Visualization",
+    description:
+      "A data science analysis of the difference in election turnout between the 2016 and 2020 presidential elections.",
+    image: "./electionTurnoutAnalysis.png",
+    link: "https://agupps.github.io/electionTurnoutAnalysis/",
+  },
+  {
+    title: "Greywater Reclamation for Hydroponics",
+    subtitle: "Mechanical Engineering Capstone",
+    description:
+      "A device that collects sink greywater and filters out contaminants and balances nutrients to optimize water for use in hydroponic systems.",
+    image: "./HydroponicsGreywaterReclamation.png",
+    link: "https://drive.google.com/drive/folders/1Gd9A5-vVY_M-JL93oCzrOF765InVwUYl?usp=sharing",
+  },
+  {
+    title: "Wombot",
+    subtitle: "Bio-Inspired Robot",
+    description:
+      "A four legged robot which walks thirty body lengths in 2 minutes",
+    image: "./Wombot.png",
+    link: "https://drive.google.com/drive/folders/1d2e9_nPcNYB0BDRP3uL2rS5hmgfCmyDG?usp=sharing",
   },
 ];
 
